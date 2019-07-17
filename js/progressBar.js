4ec2b8ee46c9639a5d1e885dee4fdbf007c43250
lhy2016 <lhy920104@gmail.com>
2019-07-17 02:06:26 -0700

    d
    d

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,7 +1,7 @@
 var container = document.getElementsByClassName("bar-container");
 
-var inputtext = ['Java','Spring','Python','Django','HTML','CSS','JS','jQuery','React','BootStrap','PHP', 'Node.js'];
-var progress = [0.80,0.7, 0.65, 0.7,0.95,0.90,0.7,0.75, 0.7,0.85, 0.65, 0.5];
+var inputtext = ['Java','Spring','Python','Django','HTML','CSS','JS','jQuery','React','BootStrap','PHP', 'MySQL'];
+var progress = [0.80,0.7, 0.65, 0.7,0.95,0.90,0.7,0.75, 0.7,0.85, 0.65, 0.8];
 
 var bars = new Array(12);
 
