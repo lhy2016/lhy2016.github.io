567f7b8a9348d1ac42905f1221f74236fd3a2a52
lhy2016 <lhy920104@gmail.com>
2019-07-31 02:41:43 -0700

    alot
    alot

--- js/main.js
+++ js/main.js
@@ -372,39 +372,52 @@ $(document).ready(function() {
 
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
-
+  $(".popup-img img").mouseover(function() {
+    $(this).css("background","linear-gradient(rgba(20,20,20, .5), rgba(20,20,20, .5))");
+  });
 
   // ========================================================================= //
   //  magnificPopup
   // ========================================================================= //
 
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
+  // var magnifPopup = function() {
+  //   $('.popup-img').magnificPopup({
+  //     type: 'image',
+  //     removalDelay: 300,
+  //     mainClass: 'mfp-with-zoom',
+  //     gallery: {
+  //       enabled: true
+  //     },
+  //     zoom: {
+  //       enabled: true, // By default it's false, so don't forget to enable it
 
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+  //       duration: 300, // duration of the effect, in milliseconds
+  //       easing: 'ease-in-out', // CSS transition easing function
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
+  //       // The "opener" function should return the element from which popup will be zoomed in
+  //       // and to which popup will be scaled down
+  //       // By defailt it looks for an image tag:
+  //       opener: function(openerElement) {
+  //         // openerElement is the element on which popup was initialized, in this case its <a> tag
+  //         // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          
+  //         return openerElement.is('img') ? openerElement : openerElement.find('img');
+  //       }
+  //     },
+  //     callbacks: {
+  //       open: function() {
+  //         console.log("opend");
+
+  //       },
+  //       close: function() {
+  //         console.log("closed");
+  //       }
+  //   // e.t.c.
+  //     }
+  //   });
+  // };
 
 
-  // Call the functions
-  magnifPopup();
+  // // Call the functions
+  // magnifPopup();
 });
