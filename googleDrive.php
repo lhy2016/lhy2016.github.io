08b35ffbb0ef48a4c3c2bd8afe8fe35806493125
lhy2016 <lhy920104@gmail.com>
2022-03-18 12:39:07 -0700

    test drive

--- googleDrive.php
+++ googleDrive.php
@@ -4,7 +4,7 @@
  $client = new Google\Client();
  $client->setAuthConfig('credentials.json');
  $client->addScope(Google\Service\Drive::DRIVE_READONLY);
- $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/googleDrive.php';
+ $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/googleDrive.php';
  $client->setRedirectUri($redirect_uri);
  $auth_url = $client->createAuthUrl();
  header('Location: ' .  filter_var($auth_url, FILTER_SANITIZE_URL));
