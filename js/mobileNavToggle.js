d29ebe4ee9582e97c9dc36d6c8e2a178f0dd5920
Haoyang Liu <lhy920104@gmail.com>
2025-08-24 21:41:31 -0700

    nav bar done (I said so, basically)
    进度感人，再这么有效率再过十年应该就能找到工作了。

--- js/mobileNavToggle.js
+++ js/mobileNavToggle.js
@@ -1,5 +1,25 @@
 
-        document.getElementById('mobile-menu-btn').addEventListener('click', function() {
-            const mobileMenu = document.getElementById('mobile-menu');
-            mobileMenu.classList.toggle('hidden');
-        });
+        const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+        const mobileMenu = document.getElementById('mobile-menu');
+        const collapsedClass = 'collapsed';
+        const logoLink = document.getElementById('logo-link');
+        
+        if (mobileMenuBtn && mobileMenu) {
+            mobileMenuBtn.addEventListener('click', () =>{
+                mobileMenuBtn.classList.toggle(collapsedClass);
+                mobileMenu.classList.toggle(collapsedClass);        
+                mobileMenu.classList.toggle('hidden');
+            });
+
+            document.querySelectorAll('#mobile-menu a, #logo-link').forEach(link => {
+                link.addEventListener('click', () => {
+                    const isMenuOpen = !mobileMenuBtn.classList.contains(collapsedClass) && !mobileMenu.classList.contains(collapsedClass);
+                    if (isMenuOpen) {
+                        mobileMenuBtn.classList.add(collapsedClass);
+                        mobileMenu.classList.add(collapsedClass);
+                        mobileMenu.classList.add('hidden');
+                    } 
+                })    
+            })
+        }
+    
