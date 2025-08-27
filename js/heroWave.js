7ce906620f6964936d5659efa601749e3dc2536d
Haoyang Liu <lhy920104@gmail.com>
2025-08-26 18:05:40 -0700

    check point before wave optimization

--- js/heroWave.js
+++ js/heroWave.js
@@ -4,6 +4,7 @@ const height = 800;
 const svg = document.getElementById("wave");
 svg.setAttribute("viewBox", `0 0 ${wavelength} ${height}`);
 
+const wavePath = document.getElementById("wave-path");
 
 const amplitude = 30;         
 const speed = 0.02;          
@@ -12,22 +13,49 @@ let phase = 0;
 
 let ripples = [];
 
-// 鼠标触发扰动
-window.addEventListener("mousemove", e => {
-  ripples.push({
-    x: e.clientX,           // 初始位置
-    strength: 10,           // 初始振幅
-    decay: 0.98,            // 每帧衰减
-    spread: 50,            // 扰动影响范围
-    drift: -8               // 水平漂移速度（负数表示向左）
-  });
+let lastMousePos = null;
+let lastTime = null;
+
+// mouse triggered ripple
+wavePath.addEventListener("mousemove", (e) => {
+
+  if (!lastMousePos) {
+    lastMousePos = { x: e.clientX, y: e.clientY};
+    return;
+  }
+
+  const dy = e.clientY - lastMousePos.y;
+  const dx = e.clientX - lastMousePos.x;
+  let strength = Math.min(Math.max(dy, -9000), 9000);
+  let spread = 80;
+  //Math.min(Math.max(Math.abs(dx), 100), 200);
+  let drift = -9;
+
+  lastMousePos = { x: e.clientX, y: e.clientY};
+
+  if (lastTime === null) {
+    
+    ripples.push({
+      x: e.clientX,
+      strength: strength,
+      decay: 0.97,
+      spread: spread,
+      drift: drift,
+    })
+    
+    
+
+    lastTime = setTimeout(() => {
+      lastTime = null;
+    }, 30);
+  }
 });
 
 function getY(x) {
 
   let y = Math.sin((x / wavelength) * 2 * Math.PI + phase) * amplitude;
 
-  // 叠加扰动
+  // add ripples
   for (let r of ripples) {
     const dx = x - r.x;
     const influence = Math.exp(-(dx * dx) / (2 * r.spread * r.spread));
@@ -37,7 +65,7 @@ function getY(x) {
   return height / 2 + y;
 }
 
-// 绘制波形
+
 function drawWave() {
   const step = wavelength / points;
   let d = `M 0 ${getY(0)}`;
@@ -45,7 +73,7 @@ function drawWave() {
     const x = i * step;
     d += ` L ${x} ${getY(x)}`;
   }
-  // 闭合到底部形成填充
+  // Close the path
   d += ` L ${wavelength} ${height} L 0 ${height} Z`;
   document.getElementById("wave-path").setAttribute("d", d);
 }
@@ -53,11 +81,11 @@ function drawWave() {
 function animate() {
   phase += speed;
 
-  // 更新并衰减扰动
+  // Update ripples
   ripples = ripples
     .map(r => ({
       ...r,
-      x: r.x + r.drift,             // 随波漂移
+      x: r.x + r.drift,             // drift horizontally
       strength: r.strength * r.decay
     }))
     .filter(r => Math.abs(r.strength) > 0.1);
